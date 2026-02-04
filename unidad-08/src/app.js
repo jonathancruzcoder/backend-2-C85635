525f6d77bb8ad9ef3623114b26745c1e74843f09
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-02-04 10:15:46 -0300

    Unidad 08 - Mailer

--- unidad-08/src/app.js
+++ unidad-08/src/app.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import cors from 'cors'
-import { config } from './config/config.js';
+import { env } from './config/config.js';
 
-
+// 
 const app = express();
 
-app.listen( config.PORT, () => {
-    console.log(`Servidor en el puerto ${config.PORT}`);
+app.use( cors() )
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Ok'
+    })
+})
+
+app.listen( env.PORT, () => {
+    console.log(`Servidor en el puerto ${env.PORT}`);
 })
